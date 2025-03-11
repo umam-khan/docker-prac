75960d1d47d13c22beeddcc7a82836c2377af665
Umam Khan <umam.khan@outlook.com>
2025-03-12 01:24:31 +0530

    setting up ci/cd for my first docker image

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Add Item nowww'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
